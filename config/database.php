a70d131bc4dcb25bcde95534760f531dd896639e
Le PAN <lpan@e1r6p20.42.fr>
2019-07-02 16:14:19 +0200

    setup database except stickers

--- config/database.php
+++ config/database.php
@@ -2,7 +2,7 @@
 $DB_HOST = 'localhost';
 $DB_USER = 'root';
 $DB_PASSWORD = 'rootroot';
-$DB_NAME = 'Camagru_db';
+$DB_NAME = 'camagru_db';
 $DB_CHARSET = 'utf8';
 
 $DB_DSN = "mysql:host={$DB_HOST};dbname={$DB_NAME};charset={$DB_CHARSET}";;
--- config/setup.php
+++ config/setup.php
@@ -3,29 +3,58 @@ require_once 'database.php';
 try{
     $settings = array(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);// set the PDO error mode to exception
     $connection = new PDO("mysql:host=localhost;$DB_CHARSET", "root", "rootroot",$settings);
-    $query = "CREATE DATABASE IF NOT EXISTS $DB_NAME";
+    $query = "CREATE DATABASE IF NOT EXISTS $DB_NAME; USE $DB_NAME;";
     $connection->exec($query);
-    echo "Database built successfully";
-}
-catch(PDOException $e){
-    echo $e->getMessage();
-}
 
-$query_users = "CREATE TABLE `users` (
-    `id_user` INT AUTO_INCREMENT PRIMARY KEY,
-    `email` VARCHAR(255) NOT NULL UNIQUE,
-    `username` VARCHAR(15) NOT NULL UNIQUE,
-    `password` VARCHAR(25) NOT NULL,
-    `email_prefer` BOOLEAN DEFAULT true,
-    `admin` BOOLEAN DEFAULT false,
-    )";
+    $query_users = "CREATE TABLE IF NOT EXISTS `users`(
+        `id_user` INT AUTO_INCREMENT PRIMARY KEY,
+        `email` VARCHAR(30) NOT NULL UNIQUE,
+        `username` VARCHAR(15) NOT NULL UNIQUE,
+        `password` VARCHAR(25) NOT NULL,
+        `email_prefer` BOOLEAN DEFAULT TRUE,
+        `admin` BOOLEAN DEFAULT FALSE
+    );";
 
-$query_imgs = "CREATE TABLE `imgs` (
-    `id_img` INT AUTO_INCREMENT PRIMARY KEY,
-    `id_user` INT NOT NULL,
-    `path` VARCHAR(1000) NOT NULL,
-    `creation_date` DATETIME DEFAULT CURRENT_TIMESTAMP(),
-)";
+    $query_imgs = "CREATE TABLE IF NOT EXISTS `imgs`(
+        `id_img` INT AUTO_INCREMENT PRIMARY KEY,
+        `id_user` INT NOT NULL,
+        `path` VARCHAR(500) NOT NULL,
+        `creation_date` DATETIME DEFAULT CURRENT_TIMESTAMP()
+    );";
 
-$connection = null;
+    $query_likes = "CREATE TABLE IF NOT EXISTS `likes`(
+        `id_like` INT AUTO_INCREMENT PRIMARY KEY,
+        `id_img` INT NOT NULL,
+        `creation_date` DATETIME DEFAULT CURRENT_TIMESTAMP()
+    );";
+
+    $query_comments = "CREATE TABLE IF NOT EXISTS `comments`(
+        `id_comment` INT AUTO_INCREMENT PRIMARY KEY,
+        `id_img` INT NOT NULL,
+        `comment` VARCHAR(500) NOT NULL,
+        `creation_date` DATETIME DEFAULT CURRENT_TIMESTAMP()
+    );";
+
+    $query_stickers = "CREATE TABLE IF NOT EXISTS `stickers`(
+        `id_sticker` INT AUTO_INCREMENT PRIMARY KEY,
+        `path` VARCHAR(500) NOT NULL
+    );";
+
+    $query_stickers_insert = "INSERT INTO `stickers` (path) VALUES(
+        
+    );";
+
+    $connection->exec($query_users);
+    $connection->exec($query_imgs);
+    $connection->exec($query_likes);
+    $connection->exec($query_comments);
+    $connection->exec($query_stickers);
+
+
+    echo "Database $DB_NAME has been built successfully";
+    }
+    catch(PDOException $e){
+        echo $e->getMessage();
+    }
+
 ?> 
